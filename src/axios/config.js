e8a123ad15ea1fc0f4ad167b7b22f703f47e6339
davibcavalcante <davib506@gmail.com>
2024-11-02 20:14:22 -0300

    Corrigindo baseUrl

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiFetch = axios.create({
-    baseURL: 'http://127.0.0.1:5000',
+    baseURL: 'https://indigitalenv.online',
     headers: {
         "Content-Type": "application/json; charset=UTF-8",
     }
