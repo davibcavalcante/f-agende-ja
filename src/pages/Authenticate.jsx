bf46683dd9bfc9d51425804d972e85eee3edd29c
davibcavalcante <davib506@gmail.com>
2024-11-02 08:58:45 -0300

    Alteração de informações

--- src/pages/Authenticate.jsx
+++ src/pages/Authenticate.jsx
@@ -8,14 +8,12 @@ const Authenticate = ({ action }) => {
         <section className="bg-authenticate bg-cover bg-fixed w-screen min-h-screen lg:flex lg:justify-center lg:p-8">
             <section className="bg-darkM hidden xl:flex xl:flex-col xl:p-8 xl:rounded-tl-xl xl:rounded-bl-xl">
                 <div className="flex flex-col max-w-2xl gap-4 flex-1">
-                    <h1 className="text-white text-4xl font-poppisn font-bold">Texto sobre a Clínica</h1>
+                    <h1 className="text-white text-4xl font-poppisn font-bold">Agende Já</h1>
                     <p className="text-white text-lg font-poppins font-light border-b-2 pb-4">
-                        Para uma mente e um corpo saudável, apresentamos a você a mais nova clínica de especialidades médicas com profissionais qualificados e a preço popular: Clínico geral, dermatologia, fisioterapia, fonoaudiologia, cardiologia, neuripsicologia, nutrição, psicologia, ortopedia entre outros.
-                        Estamos esperando sua visita. Agende consulta agora mesmo!
+                        A Agende Já é uma clínica pensada especialmente para oferecer apoio e atendimento inicial a pessoas em situação de vulnerabilidade, que muitas vezes enfrentam dificuldades para acessar o sistema de saúde. Nosso objetivo é facilitar a marcação de consultas com um clínico geral, proporcionando um primeiro contato com profissionais qualificados que podem avaliar e orientar os pacientes para o tratamento adequado.
                     </p>
                     <p className='text-white text-lg font-poppins font-light border-b-2 pb-4'>
-                        Para uma mente e um corpo saudável, apresentamos a você a mais nova clínica de especialidades médicas com profissionais qualificados e a preço popular: Clínico geral, dermatologia, fisioterapia, fonoaudiologia, cardiologia, neuripsicologia, nutrição, psicologia, ortopedia entre outros.
-                        Estamos esperando sua visita. Agende consulta agora mesmo!
+                        Na Agende Já, priorizamos a acolhida e o respeito, garantindo que cada pessoa tenha a atenção necessária para iniciar o cuidado com sua saúde. A clínica oferece agendamentos simplificados, sem burocracias, permitindo que nossos pacientes consigam acesso a uma avaliação médica básica de maneira rápida e humanizada. Além disso, contamos com horários flexíveis e profissionais comprometidos em prestar um atendimento que respeita a individualidade e as necessidades de cada um.
                     </p>
                     <div className='flex gap-4 justify-center'>
                         <p className='bg-white text-darkM font-poppins p-2 flex items-center gap-4 rounded-lg shadow-lg'>
--- src/utils/footer.js
+++ src/utils/footer.js
@@ -1,7 +1,7 @@
 export const getInformations = () => {
     return [
         {title: 'CONTATO:', texts: ['(62) 9 9999-9999', 'agendaja@gmail.com', '@agendaja_clinica']},
-        {title: 'ENDEREÇO:', texts: ['Rua Três Corações, n° 120', 'Setor Local, Goiânia-GO']},
+        {title: 'ENDEREÇO:', texts: ['Rua 12, n° 120', 'Conjunto Bom Local, Goiânia-GO']},
         {title: 'FUNCIONAMENTO:', texts: ['Seg a Sex das 08 as 22.', 'Sáb e Dom das 08 as 14.']},
     ]
 }
